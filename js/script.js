711f117e928564aa2385acd845397245ccd2daa2
Edi Aquino de Souza <edsouzza@hotmail.com>
2025-11-04 07:56:35 -0300

    atualizacao das imagens, alteracao do botao da hero, altura do card principal

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 import { scrollSmoot } from './scrollSmoot.js';
+import { modal } from './modal.js';
 
 const menu = document.querySelector('#hamburger')
 const nav = document.querySelector('#nav')
@@ -32,34 +33,4 @@ document.querySelectorAll('nav a[href^="#"]').forEach(link => {
   });
 });
 
-
-// MODAL
-const modal       = document.getElementById("modal");
-const modalImg    = document.getElementById("modal-img");
-const closeModal  = document.getElementById("close-modal");
-const fotos       = document.querySelectorAll('.foto')
-
-// pega todas as imagens clicáveis
-fotos.forEach(img => {
-  img.addEventListener("click", () => {
-    modalImg.src = img.src;           // coloca a imagem no modal
-    modal.classList.remove("hidden"); // exibe modal
-    modal.classList.add("flex");      // ativa layout
-  });
-});
-
-// fechar ao clicar no X
-closeModal.addEventListener("click", () => {
-  modal.classList.add("hidden");
-  modal.classList.remove("flex");
-});
-
-// fechar ao clicar fora da imagem
-modal.addEventListener("click", (e) => {
-  if (e.target === modal) {
-    modal.classList.add("hidden");
-    modal.classList.remove("flex");
-  }
-});
-
-
+modal()
